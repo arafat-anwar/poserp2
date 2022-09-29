26a27cf7fe3cc64921e1cc98cb53608fa57210e4
zillurcse <zillurrahman68582@gmail.com>
2022-09-29 20:21:48 +0600

    ok

--- resources/views/v2/components/widget.blade.php
+++ resources/views/v2/components/widget.blade.php
@@ -1,19 +1,34 @@
 <div class="box {{$class ?? 'box-solid'}}" @if(!empty($id)) id="{{$id}}" @endif>
     @if(empty($header))
         @if(!empty($title) || !empty($tool))
-        <div class="box-header">
-            {!!$icon ?? '' !!}
-            <h3 class="box-title">{{ $title ?? '' }}</h3>
-            {!!$tool ?? ''!!}
-        </div>
+            <div class="card card-flush py-4">
+                <!--begin::Card header-->
+                <div class="card-header">
+                    <!--begin::Card title-->
+                    {!!$icon ?? '' !!}
+                    <div class="card-title">
+                        <h2>{{ $title ?? '' }}</h2>
+                    </div>
+                    {!!$tool ?? ''!!}
+                    <!--end::Card title-->
+                </div>
+                <!--end::Card header-->
+            </div>
         @endif
     @else
         <div class="box-header">
             {!! $header !!}
         </div>
+        <div class="card-header">
+            <!--begin::Card title-->
+            <div class="card-title">
+                <h2>{!! $header !!}</h2>
+            </div>
+            <!--end::Card title-->
+        </div>
     @endif
 
-    <div class="box-body">
+    <div class="card-body">
         {{$slot}}
     </div>
     <!-- /.box-body -->
