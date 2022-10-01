efe92aae3ccc01999699f74df4857cb9b11ebfd4
zillurcse <zillurrahman68582@gmail.com>
2022-10-01 18:01:06 +0600

    ok

--- Modules/Superadmin/Resources/views/v2/packages/create.blade.php
+++ Modules/Superadmin/Resources/views/v2/packages/create.blade.php
@@ -3,15 +3,15 @@
 
 @section('content')
 
-
+<div class="form d-flex flex-column flex-lg-row">
+<div class="d-flex flex-column flex-row-fluid gap-7 gap-lg-10 card card-flush p-5">
 <!-- Content Header (Page header) -->
-<section class="content-header">
-    <h1>@lang('superadmin::lang.packages') <small>@lang('superadmin::lang.add_package')</small></h1>
-    <!-- <ol class="breadcrumb">
-        <li><a href="#"><i class="fa fa-dashboard"></i> Level</a></li>
-        <li class="active">Here</li>
-    </ol> -->
-</section>
+	<div class="page-title d-flex flex-column justify-content-center flex-wrap me-3">
+		<!--begin::Title-->
+		<h1 class="page-heading d-flex text-dark fw-bold fs-3 flex-column justify-content-center my-0">@lang('superadmin::lang.packages') <small>@lang('superadmin::lang.add_package')</small></h1>
+		<!--end::Title-->
+		<!--end::Breadcrumb-->
+	</div>
 
 <!-- Main content -->
 <section class="content">
@@ -28,25 +28,25 @@
 		<div class="box-body">
 			<div class="row">
 				
-				<div class="col-sm-6">
+				<div class="col-sm-6 mb-3 fv-row">
 					<div class="form-group">
-						{!! Form::label('name', __('messages.name').':') !!}
-						{!! Form::text('name', null, ['class' => 'form-control', 'required']); !!}
+						{!! Form::label('name', __('messages.name').':', 'class="form-label ppt-2 mt-2"') !!}
+						{!! Form::text('name', null, ['class' => 'form-control', 'placeholder' => __('messages.name'), 'required']); !!}
 					</div>
 				</div>
 
-				<div class="col-sm-6">
+				<div class="col-sm-6 mb-3 fv-row">
 					<div class="form-group">
-						{!! Form::label('description', __('superadmin::lang.description').':') !!}
-						{!! Form::text('description', null, ['class' => 'form-control', 'required']); !!}
+						{!! Form::label('description', __('superadmin::lang.description').':', 'class="form-label ppt-2 mt-2"') !!}
+						{!! Form::text('description', null, ['class' => 'form-control', 'placeholder' => __('superadmin::lang.description'), 'required']); !!}
 					</div>
 				</div>
 
 				<div class="clearfix"></div>
-				<div class="col-sm-6">
+				<div class="col-sm-6 mb-3 fv-row">
 					<div class="form-group">
-						{!! Form::label('location_count', __('superadmin::lang.location_count').':') !!}
-						{!! Form::number('location_count', null, ['class' => 'form-control', 'required', 'min' => 0]); !!}
+						{!! Form::label('location_count', __('superadmin::lang.location_count').':', 'class="form-label ppt-2 mt-2"') !!}
+						{!! Form::number('location_count', null, ['class' => 'form-control','placeholder' => __('superadmin::lang.location_count'), 'required', 'min' => 0]); !!}
 
 						<span class="help-block">
 							@lang('superadmin::lang.infinite_help')
@@ -54,33 +54,10 @@
 					</div>
 				</div>
 
-				<div class="col-sm-6">
+				<div class="col-sm-6 mb-3 fv-row">
 					<div class="form-group">
-						{!! Form::label('user_count', __('superadmin::lang.user_count').':') !!}
-						{!! Form::number('user_count', null, ['class' => 'form-control', 'required', 'min' => 0]); !!}
-
-						<span class="help-block">
-							@lang('superadmin::lang.infinite_help')
-						</span>
-					</div>
-				</div>
-				<div class="clearfix"></div>
-
-				<div class="col-sm-6">
-					<div class="form-group">
-						{!! Form::label('product_count', __('superadmin::lang.product_count').':') !!}
-						{!! Form::number('product_count', null, ['class' => 'form-control', 'required', 'min' => 0]); !!}
-
-						<span class="help-block">
-							@lang('superadmin::lang.infinite_help')
-						</span>
-					</div>
-				</div>
-
-				<div class="col-sm-6">
-					<div class="form-group">
-						{!! Form::label('invoice_count', __('superadmin::lang.invoice_count').':') !!}
-						{!! Form::number('invoice_count', null, ['class' => 'form-control', 'required', 'min' => 0]); !!}
+						{!! Form::label('user_count', __('superadmin::lang.user_count').':', 'class="form-label ppt-2 mt-2"') !!}
+						{!! Form::number('user_count', null, ['class' => 'form-control','placeholder' => __('superadmin::lang.user_count'), 'required', 'min' => 0]); !!}
 
 						<span class="help-block">
 							@lang('superadmin::lang.infinite_help')
@@ -89,54 +66,83 @@
 				</div>
 				<div class="clearfix"></div>
 
-				<div class="col-sm-6">
+				<div class="col-sm-6 mb-3 fv-row">
 					<div class="form-group">
-						{!! Form::label('interval', __('superadmin::lang.interval').':') !!}
+						{!! Form::label('product_count', __('superadmin::lang.product_count').':', 'class="form-label ppt-2 mt-2"') !!}
+						{!! Form::number('product_count', null, ['class' => 'form-control','placeholder' => __('superadmin::lang.product_count'), 'required', 'min' => 0]); !!}
+
+						<span class="help-block">
+							@lang('superadmin::lang.infinite_help')
+						</span>
+					</div>
+				</div>
+
+				<div class="col-sm-6 mb-3 fv-row">
+					<div class="form-group">
+						{!! Form::label('invoice_count', __('superadmin::lang.invoice_count').':', 'class="form-label ppt-2 mt-2"') !!}
+						{!! Form::number('invoice_count', null, ['class' => 'form-control','placeholder' => __('superadmin::lang.invoice_count'), 'required', 'min' => 0]); !!}
+
+						<span class="help-block">
+							@lang('superadmin::lang.infinite_help')
+						</span>
+					</div>
+				</div>
+				<div class="clearfix"></div>
+
+				<div class="col-sm-6 mb-3 fv-row">
+					<div class="form-group">
+						{!! Form::label('interval', __('superadmin::lang.interval').':', 'class="form-label ppt-2 mt-2"') !!}
 
 						{!! Form::select('interval', $intervals, null, ['class' => 'form-control select2', 'placeholder' => __('messages.please_select'), 'required']); !!}
 					</div>
 				</div>
 
-				<div class="col-sm-6">
+				<div class="col-sm-6 mb-3 fv-row">
 					<div class="form-group">
-						{!! Form::label('interval_count	', __('superadmin::lang.interval_count').':') !!}
-						{!! Form::number('interval_count', null, ['class' => 'form-control', 'required', 'min' => 1]); !!}
+						{!! Form::label('interval_count	', __('superadmin::lang.interval_count').':', 'class="form-label ppt-2 mt-2"') !!}
+						{!! Form::number('interval_count', null, ['class' => 'form-control','placeholder' => __('superadmin::lang.interval_count'), 'required', 'min' => 1]); !!}
 					</div>
 				</div>
 				<div class="clearfix"></div>
 
-				<div class="col-sm-6">
+				<div class="col-sm-6 mb-3 fv-row">
 					<div class="form-group">
-						{!! Form::label('trial_days	', __('superadmin::lang.trial_days').':') !!}
-						{!! Form::number('trial_days', null, ['class' => 'form-control', 'required', 'min' => 0]); !!}
+						{!! Form::label('trial_days	', __('superadmin::lang.trial_days').':', 'class="form-label ppt-2 mt-2"') !!}
+						{!! Form::number('trial_days', null, ['class' => 'form-control','placeholder' => __('superadmin::lang.trial_days'), 'required', 'min' => 0]); !!}
 					</div>
 				</div>
+{{--				<div class="input-group mb-5">--}}
+{{--					<input type="text" class="form-control" placeholder="Recipient's username" aria-label="Recipient's username" aria-describedby="basic-addon2"/>--}}
+{{--					<span class="input-group-text" id="basic-addon2">--}}
+{{--						<i class="fas fa-envelope-open-text fs-4"></i>--}}
+{{--					</span>--}}
+{{--				</div>--}}
 
-				<div class="col-sm-6">
+				<div class="col-sm-6 mb-3 fv-row">
 					<div class="form-group">
-						{!! Form::label('price', __('superadmin::lang.price').':') !!}
+						{!! Form::label('price', __('superadmin::lang.price').':', 'class="form-label pt-2 mt-2"') !!}
 						@show_tooltip(__('superadmin::lang.tooltip_pkg_price'))
 
 						<div class="input-group">
-							<span class="input-group-addon" id="basic-addon3"><b>{{$currency->code}} {{$currency->symbol}}</b></span>
+							<span class="input-group-text" id="basic-addon2"><b>{{$currency->code}} {{$currency->symbol}}</b></span>
 							{!! Form::text('price', null, ['class' => 'form-control input_number', 'required']); !!}
 						</div>
-						<span class="help-block">
+						<span class="form-label">
 							0 = @lang('superadmin::lang.free_package')
 						</span>
 					</div>
 				</div>
 				<div class="clearfix"></div>
 
-				<div class="col-sm-6">
+				<div class="col-sm-6 mb-3 fv-row">
 					<div class="form-group">
-						{!! Form::label('sort_order	', __('superadmin::lang.sort_order').':') !!}
+						{!! Form::label('sort_order	', __('superadmin::lang.sort_order').':', 'class="form-label pt-2 mt-2"') !!}
 						{!! Form::number('sort_order', 1, ['class' => 'form-control', 'required']); !!}
 					</div>
 				</div>
 
 				<div class="clearfix"></div>
-				<div class="col-sm-6">
+				<div class="col-sm-6 mb-3 fv-row">
 					<div class="checkbox">
 					<label>
 						{!! Form::checkbox('is_private', 1, false, ['class' => 'input-icheck']); !!}
@@ -145,7 +151,7 @@
 					</div>
 				</div>
 
-				<div class="col-sm-6">
+				<div class="col-sm-6 mb-3 fv-row">
 					<div class="checkbox">
 					<label>
 						{!! Form::checkbox('is_one_time', 1, false, ['class' => 'input-icheck']); !!}
@@ -165,31 +171,40 @@
 				<div id="custom_link_div" class="hide">
 					<div class="col-sm-4">
 						<div class="form-group">
-							{!! Form::label('custom_link', __('superadmin::lang.custom_link').':') !!}
+							{!! Form::label('custom_link', __('superadmin::lang.custom_link').':', 'class="form-label pt-2 mt-2"') !!}
 							{!! Form::text('custom_link', null, ['class' => 'form-control']); !!}
 						</div>
 					</div>
 					<div class="col-sm-4">
 						<div class="form-group">
-							{!! Form::label('custom_link_text', __('superadmin::lang.custom_link_text').':') !!}
+							{!! Form::label('custom_link_text', __('superadmin::lang.custom_link_text').':', 'class="form-label pt-2 mt-2"') !!}
 							{!! Form::text('custom_link_text', null, ['class' => 'form-control']); !!}
 						</div>
 					</div>
 				</div>
 				<div class="clearfix"></div>
+				<div class="row mt-5 mb-3 gap-0">
 				@foreach($permissions as $module => $module_permissions)
 					@foreach($module_permissions as $permission)
-					<div class="col-sm-3">
-						<div class="checkbox">
-						<label>
-							{!! Form::checkbox("custom_permissions[$permission[name]]", 1, $permission['default'], ['class' => 'input-icheck']); !!}
-	                        {{$permission['label']}}
-						</label>
-						</div>
-					</div>
+							<div class="col-sm-2">
+								{!! Form::checkbox("custom_permissions[$permission[name]]", 1, $permission['default'], ['class' => 'form-check-input']); !!}
+{{--								<input class="form-check-input" type="checkbox" value="1" id="flexCheckDefault"/>--}}
+								<label class="form-check-label" for="flexCheckDefault">
+									{{$permission['label']}}
+								</label>
+							</div>
+
+{{--					<div class="row d-flex">--}}
+{{--						<div class="checkbox">--}}
+{{--						<label>--}}
+{{--							{!! Form::checkbox("custom_permissions[$permission[name]]", 1, $permission['default'], ['class' => 'input-icheck']); !!}--}}
+{{--	                        {{$permission['label']}}--}}
+{{--						</label>--}}
+{{--						</div>--}}
+{{--					</div>--}}
 					@endforeach
 				@endforeach
-
+				</div>
 				<div class="col-sm-3">
 					<div class="checkbox">
 					<label>
@@ -198,8 +213,6 @@
 					</label>
 					</div>
 				</div>
-
-				
 			</div>
 
 			<div class="row">
@@ -213,7 +226,8 @@
 
 	{!! Form::close() !!}
 </section>
-
+</div>
+</div>
 @endsection
 
 @section('javascript')
