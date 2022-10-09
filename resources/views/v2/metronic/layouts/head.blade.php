7633840505248cc089c66d0b7cf767b04bd54dbb
gmfaruk <gmfaruk2021@gmail.com>
2022-10-09 21:27:11 +0600

    roles crud design
    Note: Need to check input:checkbox

--- resources/views/v2/metronic/layouts/head.blade.php
+++ resources/views/v2/metronic/layouts/head.blade.php
@@ -30,6 +30,7 @@
 	<link href="{{ asset('metronic/assets') }}/plugins/global/plugins.bundle.css" rel="stylesheet" type="text/css" />
 	<link href="{{ asset('metronic/assets') }}/css/style.bundle.css" rel="stylesheet" type="text/css" />
 	<link href="{{ asset('metronic/assets') }}/css/style.custom.css" rel="stylesheet" type="text/css" />
+	{{-- <link href="{{ asset('css') }}/vendor.css" rel="stylesheet" type="text/css" /> --}}
 	<!--end::Global Stylesheets Bundle-->
 
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css"/>
--- resources/views/v2/role/partials/module_permissions.blade.php
+++ resources/views/v2/role/partials/module_permissions.blade.php
@@ -7,7 +7,7 @@
   @endphp
   @foreach($module_permissions as $key => $value)
 
-  <div class="row check_group">
+  <div class="row check_group mt-2">
     <div class="col-md-3">
       <h4>@lang(strtolower($key).'::lang.'.strtolower($key))</h4>
     </div>
@@ -19,10 +19,11 @@
         }
       @endphp
       <div class="col-md-12">
-        <div class="checkbox">
-          <label>
+        <div class="checkbox-list">
+          <label class="checkbox">
             {!! Form::checkbox('permissions[]', $module_permission['value'], in_array($module_permission['value'], $module_role_permissions), 
             [ 'class' => 'input-icheck']); !!} {{ $module_permission['label'] }}
+            <span></span>
           </label>
         </div>
       </div>
